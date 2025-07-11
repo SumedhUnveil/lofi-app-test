f7b70e97b2632c80e0d936f45de19a09589e6ee2
SumedhUnveil <sumedh.bajracharya@unveil.health>
2025-07-11 14:34:13 +0545

    Working app

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
-// import Player from './components/Player';
-// import { ThemeProvider } from './context/ThemeContext';
+import Player from './components/Player';
+import { ThemeProvider } from './context/ThemeContext';
 import './index.css';
 
 const App: React.FC = () => {
   return (
-    // <ThemeProvider>
-    //   <Player />
-    // </ThemeProvider>
-    <>Hello</>
+    <ThemeProvider>
+      <Player />
+    </ThemeProvider>
   );
 };
 
