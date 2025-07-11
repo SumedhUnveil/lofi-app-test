a3a659ae3f8f669f8387e902cace9706d5d21173
SumedhUnveil <sumedh.bajracharya@unveil.health>
2025-07-11 14:04:38 +0545

    Static test

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import Player from './components/Player';
-import { ThemeProvider } from './context/ThemeContext';
+// import Player from './components/Player';
+// import { ThemeProvider } from './context/ThemeContext';
 import './index.css';
 
 const App: React.FC = () => {
   return (
-    <ThemeProvider>
-      <Player />
-    </ThemeProvider>
+    // <ThemeProvider>
+    //   <Player />
+    // </ThemeProvider>
+    <>Hello</>
   );
 };
 
